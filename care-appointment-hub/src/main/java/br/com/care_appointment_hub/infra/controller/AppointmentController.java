fb4e25e129bea2eed9bed47c5f1f83fec5cdc38a
Guilherme Lima <gslima0806@gmail.com>
2026-05-13 10:57:12 -0300

    adicionando controle de versão

--- care-appointment-hub/src/main/java/br/com/care_appointment_hub/infra/controller/AppointmentController.java
+++ care-appointment-hub/src/main/java/br/com/care_appointment_hub/infra/controller/AppointmentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/appointments")
+@RequestMapping("/api/v1/appointments")
 public class AppointmentController {
     private final CreateAppointmentUseCase createUseCase;
     private final UpdateAppointmentUseCase updateUseCase;
